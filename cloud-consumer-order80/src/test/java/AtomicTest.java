9fa640a9a855a49a99018cd1647368f26e908e1e
zhangkang11223 <1147867830@qq.com>
2020-10-14 19:55:42 +0800

    自定义负载均衡轮询算法

--- cloud-consumer-order80/src/test/java/AtomicTest.java
+++ cloud-consumer-order80/src/test/java/AtomicTest.java
@@ -2,7 +2,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class AtomicTest {
     public static void main(String[] args) {
-
         AtomicInteger atomicInteger = new AtomicInteger(0);
         boolean b = atomicInteger.compareAndSet(0, 1);
         System.out.println(b);
